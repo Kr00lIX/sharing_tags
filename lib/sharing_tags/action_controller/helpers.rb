a50d5f8de68150c4e2f9a2fb218d026b2b2e4485
Anatoliy Kovalchuk <kr00lix@gmail.com>
2015-04-17 15:39:07 +0700

    add sharing_tags_context helper method

--- lib/sharing_tags/action_controller/helpers.rb
+++ lib/sharing_tags/action_controller/helpers.rb
@@ -4,4 +4,8 @@ module SharingTags::ActionController::Helpers
     SharingTags.config.within_context_params(view_context)
   end
 
+  def sharing_tags_context(name, *attrs)
+    SharingTags.config.switch_context(name, *attrs)
+  end
+
 end
