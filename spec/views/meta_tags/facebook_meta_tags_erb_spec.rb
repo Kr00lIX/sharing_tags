afa2c6cee855b7c1e223f5b36cf74b3e2dfdda6b
Anatoliy Kovalchuk <kr00lix@gmail.com>
2015-06-29 20:37:27 +0700

    render caption meta tag for blank title

--- spec/views/meta_tags/facebook_meta_tags_erb_spec.rb
+++ spec/views/meta_tags/facebook_meta_tags_erb_spec.rb
@@ -58,4 +58,37 @@ RSpec.describe "sharing_tags/meta_tags", type: :view do
       expect(rendered).to have_tag "meta", with: {property: "og:url",          content: "http://a.b"}
     end
   end
+
+  describe "doesn't render empty params" do
+    before do
+      controller.append_view_path 'app/views'
+
+      SharingTags.configure do
+        share_url "http:://dummy.com"
+
+        facebook do
+          caption       { "Facebook caption" }
+          image_url     { "http://img.jpg" }
+        end
+      end
+    end
+
+    it "render caption instead title" do
+      render
+      expect(rendered).to have_tag "meta", with: {property: "og:title",        content: "Facebook caption"}
+    end
+
+    it "expect render image meta" do
+      render
+      expect(rendered).to have_tag "meta", with: {property: "og:image",        content: "http://img.jpg"}
+    end
+
+    it "shouldn't render empty params" do
+      render
+      expect(rendered).to_not have_tag "meta", with: {property: "og:description"}
+      expect(rendered).to_not have_tag "meta", with: {property: "og:image:type"}
+      expect(rendered).to_not have_tag "meta", with: {property: "og:image:width"}
+      expect(rendered).to_not have_tag "meta", with: {property: "og:image:height"}
+    end
+  end
 end
